356e38a0fbbc87c408b0fe43d4c91ec64cc4f7d8
HDuck <1ntranc3d@gmail.com>
2018-10-17 19:56:26 +0300

    Modified gulp-build to look prettier

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,27 @@
 const gulp = require('gulp'),
       pug = require('gulp-pug'),
       scss = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer'),
+      cssMinify = require('gulp-csso'),
+      uglify = require('gulp-uglify-es').default,
       clean = require('gulp-clean'),
       concat = require('gulp-concat'),
       copy = require('gulp-copy'),
-      cssMinify = require('gulp-csso'),
       rename = require('gulp-rename');
 
 gulp.task('scss', () => {
     gulp.src('./src/scss/main.scss')
         .pipe(scss().on('error', scss.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest('./src/css'));
 });
 
 gulp.task('pug', () => {
-    return gulp.src('./src/pug/main.pug')
-        .pipe(pug({
-            pretty: false,
-        }))
+    gulp.src('./src/pug/main.pug')
+        .pipe(pug({ pretty: false }))
         .pipe(rename((path) => {
             path.basename = 'index'
         }))
@@ -25,49 +29,45 @@ gulp.task('pug', () => {
 });
 
 gulp.task('concat:css', () => {
-    return gulp.src(['./src/css/normalize.css', './src/css/main.css'])
+    gulp.src(['./src/css/normalize.css', './src/css/main.css'])
         .pipe(concat('styles.css'))
         .pipe(gulp.dest('./src/css'));
 });
 
 gulp.task('watch', () => {
-    return gulp.watch(['./src/pug/*.pug', './src/scss/*.scss'], ['pug', 'scss']);
+    gulp.watch(['./src/pug/*.pug', './src/scss/*.scss'], ['pug', 'scss']);
 });
 
 gulp.task('clean', () => {
-    return gulp.src('./docs')
+    gulp.src('./docs')
         .pipe(clean({force: true}));
 });
 
-gulp.task('html2docs', () => {
-    return gulp.src('./src/index.html')
-    .pipe(copy('./docs', {prefix: 1}))
-    .pipe(gulp.dest('./docs'));
-});
-
-gulp.task('css2docs', () => {
-    return gulp.src('./src/css/styles.css')
-    .pipe(cssMinify())
-    .pipe(copy('./docs', {prefix: 1}))
-    .pipe(gulp.dest('./docs'));
-});
-
-gulp.task('img2docs', () => {
-    return gulp.src('./src/img/*.png')
-        .pipe(copy('./docs', {prefix: 1}))
+gulp.task('copy2docs', () => {
+    gulp.src([
+        './src/index.html',
+        './src/css/styles.min.css',
+        './src/js/script.min.js',
+        './src/fonts/*.otf',
+        './src/img/*.png'])
+        .pipe(copy('./docs', { prefix: 1 }))
         .pipe(gulp.dest('./docs'));
 });
 
-gulp.task('font2docs', () => {
-    return gulp.src('./src/fonts/*.otf')
-        .pipe(copy('./docs', {prefix: 1}))
-        .pipe(gulp.dest('./docs'));
+gulp.task('uglify-js', () => {
+    gulp.src('./src/js/script.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./src/js'));
 });
 
-gulp.task('js2docs', () => {
-    return gulp.src('./src/js/*.js')
-        .pipe(copy('./docs', {prefix: 1}))
-        .pipe(gulp.dest('./docs'));
+gulp.task('minify-css', () => {
+    gulp.src('./src/css/styles.css')
+        .pipe(cssMinify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./src/css'))
 });
 
-gulp.task('build', ['pug', 'html2docs', 'scss', 'concat:css', 'css2docs', 'img2docs', 'font2docs', 'js2docs']);
+gulp.task('css', ['scss', 'concat:css']);
+
+gulp.task('build', ['clean', 'pug', 'scss', 'concat:css', 'minify-css', 'uglify-js', 'copy2docs']);
